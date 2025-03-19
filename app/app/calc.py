dd3ccabde1a6f8d1f6ac8f6bc52ffd9bf7ada982
AmirScheldon <amirtaslimi12@gmail.com>
2025-03-19 15:08:51 +0330

    Added database adaptor and a sample test

---
+++ app/app/calc.py
@@ -0,0 +1,14 @@
+"""Calculate functions"""
+
+
+def add(x, y):
+    """Add x and y then return r"""
+    
+    r = x + y
+    return r
+
+def subtract(x, y):
+    """Subtract x from y then retun r"""
+    
+    r = y - x
+    return r
