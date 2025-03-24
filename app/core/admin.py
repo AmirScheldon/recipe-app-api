49bf2f02db137780173aa2ceb54cadf94bb9b6d7
AmirScheldon <amirtaslimi12@gmail.com>
2025-03-24 13:56:34 +0330

    Added customized admin panel.

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,1 +1,45 @@
-from django.contrib import admin  # noqa
+"""Django admin customizations."""
+
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+
+from . import models
+
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ["username", "name", "email"]
+    fieldsets = (
+        (None, {"fields": ("username", "email", "password")}),
+        (
+            _("permisions"),
+            {
+                "fields": (
+                    "is_staff",
+                    "is_active",
+                    "is_superuser",
+                )
+            }
+        ),
+        (_("Important dates"), {"fields": ("last_login",)})
+    )
+    readonly_fields = ["last_login"]
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": (
+                "username",
+                "email",
+                "password1",
+                "password2",
+                "name",
+                "is_active",
+                "is_staff",
+                "is_superuser",
+            )
+        }),
+    )
+
+
+admin.site.register(models.User, UserAdmin)
